6b65ee0003372264339996b400942b8dffd70ba1
ash47 <ash.fourtyseven@gmail.com>
2014-09-17 11:46:58 +1000

    Got basic physics going

--- core/src/com/teamlemmings/lemmings/screens/GameScreen.java
+++ core/src/com/teamlemmings/lemmings/screens/GameScreen.java
@@ -1,45 +1,121 @@
 package com.teamlemmings.lemmings.screens;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.teamlemmings.lemmings.Constants;
 
 public class GameScreen extends LemmingScreen {
 	private Box2DDebugRenderer debugRenderer;
 	private World world;
 	
-	private OrthographicCamera camera;
+	private OrthographicCamera cam;
+	
+	private float accumulator = 0;
 	
 	public GameScreen(Game game) {
 		super(game);
-		
-		// Create a physics world and debug renderer
-		// We need to replace the debug renderer with
-		//  actual graphics at some point
-		this.world = new World(new Vector2(0, -10), true);
-		this.debugRenderer = new Box2DDebugRenderer();
-		
-		// Create the camera
-		this.camera = new OrthographicCamera(24, 16);
 	}
 	
 	@Override
 	public void show () {
+		// Create a physics world and debug renderer
+		// We need to replace the debug renderer with
+		//  actual graphics at some point
+		world = new World(new Vector2(0, -10), true);
+		debugRenderer = new Box2DDebugRenderer();
 		
+		// Create the camera
+		cam = new OrthographicCamera(200, 150);
+		
+		
+		// First we create a body definition
+		BodyDef bodyDef = new BodyDef();
+		// We set our body to dynamic, for something like ground which doesn't move we would set it to StaticBody
+		bodyDef.type = BodyType.DynamicBody;
+		// Set our body's starting position in the world
+		bodyDef.position.set(0, 10);
+
+		// Create our body in the world using our body definition
+		Body body = world.createBody(bodyDef);
+
+		// Create a circle shape and set its radius to 6
+		CircleShape circle = new CircleShape();
+		circle.setRadius(6f);
+
+		// Create a fixture definition to apply our shape to
+		FixtureDef fixtureDef = new FixtureDef();
+		fixtureDef.shape = circle;
+		fixtureDef.density = 0.5f; 
+		fixtureDef.friction = 0.4f;
+		fixtureDef.restitution = 0.6f; // Make it bounce a little bit
+
+		// Create our fixture and attach it to the body
+		body.createFixture(fixtureDef);
+
+		// Remember to dispose of any shapes after you're done with them!
+		// BodyDef and FixtureDef don't need disposing, but shapes do.
+		circle.dispose();
+		
+		
+		
+		
+		
+		
+		// Create our body definition
+		BodyDef groundBodyDef = new BodyDef();  
+		// Set its world position
+		groundBodyDef.position.set(new Vector2(0, -50));  
+
+		// Create a body from the defintion and add it to the world
+		Body groundBody = world.createBody(groundBodyDef);  
+
+		// Create a polygon shape
+		PolygonShape groundBox = new PolygonShape();  
+		// Set the polygon shape as a box which is twice the size of our view port and 20 high
+		// (setAsBox takes half-width and half-height as arguments)
+		groundBox.setAsBox(cam.viewportWidth, 10.0f);
+		// Create a fixture from our polygon shape and add it to our ground body  
+		groundBody.createFixture(groundBox, 0.0f); 
+		// Clean up after ourselves
+		groundBox.dispose();
 	}
 
 	@Override
 	public void render(float delta) {
+		// Reset the background
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		// Update the camera
-		camera.update();
+		cam.update();
 		
 		// Update the physics world
-		world.step(1/60f, 6, 2);
+		doPhysicsStep(delta);
 		
 		// Render the world
-		debugRenderer.render(world, camera.combined);
+		debugRenderer.render(world, cam.combined);
 	}
 
+	private void doPhysicsStep(float deltaTime) {
+	    // fixed time step
+	    // max frame time to avoid spiral of death (on slow devices)
+	    float frameTime = Math.min(deltaTime, 0.25f);
+	    accumulator += frameTime;
+	    while (accumulator >= Constants.TIME_STEP) {
+	        world.step(Constants.TIME_STEP, Constants.VELOCITY_ITERATIONS, Constants.POSITION_ITERATIONS);
+	        accumulator -= Constants.TIME_STEP;
+	    }
+	}
 }
