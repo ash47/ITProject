df6871497f2613ab578c25951aab41c3c7a71df1
ash47 <ash.fourtyseven@gmail.com>
2014-09-24 16:39:34 +1000

    Begun to add the InteractiveObject class

---
+++ core/src/com/teamlemmings/lemmings/gameobjects/InteractiveObject.java
@@ -0,0 +1,14 @@
+package com.teamlemmings.lemmings.gameobjects;
+
+import com.teamlemmings.lemmings.screens.GameScreen;
+
+/**
+ * This class represents any objects that the user interacts with using gestures
+ * @author aschmid
+ *
+ */
+public class InteractiveObject extends GameObject {
+	public InteractiveObject(GameScreen screen, float x, float y) {
+		super(screen, x, y);
+	}
+}
