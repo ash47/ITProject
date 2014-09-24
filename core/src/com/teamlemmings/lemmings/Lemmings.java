a659ce4208948506a7ab079060f416a1e55b6dce
ash47 <ash.fourtyseven@gmail.com>
2014-09-24 15:52:55 +1000

    Added documenation for Lemmings class

--- core/src/com/teamlemmings/lemmings/Lemmings.java
+++ core/src/com/teamlemmings/lemmings/Lemmings.java
@@ -5,18 +5,30 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.teamlemmings.lemmings.screens.GameScreen;
 
+/**
+ * This is the common entry point for our game on all platforms
+ * @author aschmid
+ *
+ */
 public class Lemmings extends Game {
+	// Whether the game is being compiled for release, it will turn off logging if it's on
 	public static final boolean isRelease = false;
 	
 	@Override
 	public void create () {
+		// Change to the game screen
 		setScreen(new GameScreen(this));
 	}
 	
+	/**
+	 * Sets the log level based on release mode
+	 */
 	public void setLogLevel() {
-		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		// Set to the appropriate logging level
 		if(isRelease) {
 			Gdx.app.setLogLevel(Application.LOG_NONE);
-		}	
+		} else {
+			Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		}
 	}
 }
