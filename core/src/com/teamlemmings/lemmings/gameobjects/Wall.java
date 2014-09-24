6654c5662f8e367104cd0708db8a4c422d0d656e
ash47 <ash.fourtyseven@gmail.com>
2014-09-24 16:03:54 +1000

    Added documentation for the wall class

--- core/src/com/teamlemmings/lemmings/gameobjects/Wall.java
+++ core/src/com/teamlemmings/lemmings/gameobjects/Wall.java
@@ -7,13 +7,26 @@ import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.teamlemmings.lemmings.Constants;
 import com.teamlemmings.lemmings.screens.GameScreen;
 
+/**
+ * This class represents a basic wall
+ * @author aschmid
+ *
+ */
 public class Wall extends GameObject {
-	// Width of this wall
+	// Width of this wall (meters)
 	private float width;
 	
-	// Height of this wall
+	// Height of this wall (meters)
 	private float height;
 	
+	/**
+	 * Create a new wall of the given size
+	 * @param screen The screen to attach to
+	 * @param x The x position of the screen
+	 * @param y The y position of the screen
+	 * @param w The width of the wall
+	 * @param h The height of the wall
+	 */
 	public Wall(GameScreen screen, float x, float y, float w, float h) {
 		// Setup the game object
 		super(screen, x, y);
@@ -27,11 +40,13 @@ public class Wall extends GameObject {
 	}
 	
 	@Override
-	public void render(float deltaTime, Batch batch) {}
+	public void render(float deltaTime, Batch batch) {
+		// This needs to be implemented
+	}
 	
 	@Override
 	protected void createFixture() {
-		// Ensure vars are actually set
+		// Ensure vars are actually set, if not, don't setup physics
 		if(this.width <= 0 || this.height <= 0) return;
 		
 		// Create a polygon shape
